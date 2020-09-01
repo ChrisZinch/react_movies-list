64f910d46314e70b2d9c77f228c07bd992630bd2
ChrisZinch <chrispakulyak@gmail.com>
2020-09-01 15:47:43 +0300

    Updates

---
+++ precache-manifest.28a1e43da5d4a0741ae3dc346558e10a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3ce3484793f70c6ecd82a4312582b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5131f936d521ae25616a",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "fe929d559113dd1609fe",
+    "url": "./static/css/main.8a2ebc01.chunk.css"
+  },
+  {
+    "revision": "5131f936d521ae25616a",
+    "url": "./static/js/2.a151ed51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a151ed51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe929d559113dd1609fe",
+    "url": "./static/js/main.a87e606b.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
